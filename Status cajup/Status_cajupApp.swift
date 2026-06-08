fb65fddfb15965d8398653d82ebc3626cb623177
martinhafizi <hafizi.martin@gmail.com>
2026-06-08 19:49:03 +0200

    Initial Commit

---
+++ Status cajup/Status_cajupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Status_cajupApp.swift
+//  Status cajup
+//
+//  Created by Martin Hafizi on 8.6.26.
+//
+
+import SwiftUI
+
+@main
+struct Status_cajupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Status cajupTests/Status_cajupTests.swift
@@ -0,0 +1,19 @@
+//
+//  Status_cajupTests.swift
+//  Status cajupTests
+//
+//  Created by Martin Hafizi on 8.6.26.
+//
+
+import Testing
+@testable import Status_cajup
+
+struct Status_cajupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
